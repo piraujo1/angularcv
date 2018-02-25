7401e178f58acfd31d25d6ddc7ead1f16957a56b
Jonathan Pirau <pirauj@gmail.com>
2018-02-25 22:05:17 +0100

    exp

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -8,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class CvComponent implements OnInit {
 
   mypict: string;
+  arhspict: string;
+  unifrpict: string;
+  unamurpict: string;
 
   constructor() {
     this.mypict='assets/pictures/meDessin.jpg';
+    this.arhspict='assets/pictures/arhs-cube.png';
+    this.unifrpict='assets/pictures/unifr.png';
+    this.unamurpict='assets/pictures/unamur.png';
   }
 
   ngOnInit() {
+  }
+
+  golinkedin() {
+    window.open('https://www.linkedin.com/in/jonathan-pirau-918631114/',"_blank");
   }
 
 }
